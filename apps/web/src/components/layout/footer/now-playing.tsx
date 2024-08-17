1afb648ed484a0b2619b5bad5d23a6d0b27f884d
Ali Imam <visual.imam@gmail.com>
2024-08-17 23:15:00 +0530

    bg

--- apps/web/src/components/layout/footer/now-playing.tsx
+++ apps/web/src/components/layout/footer/now-playing.tsx
@@ -1,9 +1,8 @@
-// @ts-nocheck
-
 import type { Artist, NowPlayingSong } from "@/types";
 import Link from "next/link";
 import { getNowPlaying } from "@/lib/spotify";
 
+import { AuroraBackground } from "../../ui/aurora-bg";
 import AnimatedBars from "./animatedBars";
 
 async function fetchNowPlaying(): Promise<NowPlayingSong | null> {
@@ -46,31 +45,7 @@ export default async function NowPlaying() {
 
   if (!nowPlaying) {
     return (
-      <div className="relative overflow-hidden">
-        <div
-          aria-hidden="true"
-          className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-            }}
-            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-          />
-        </div>
-        <div
-          aria-hidden="true"
-          className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-        >
-          <div
-            style={{
-              clipPath:
-                "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-            }}
-            className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-          />
-        </div>
+      <AuroraBackground classID="">
         <div className="flex items-center justify-center px-6 py-2 text-center text-sm">
           <div className="flex justify-center px-2">
             <svg className="h-4 w-4" viewBox="0 0 168 168">
@@ -94,36 +69,12 @@ export default async function NowPlaying() {
             </Link>
           </div>
         </div>
-      </div>
+      </AuroraBackground>
     );
   }
 
   return (
-    <div className="relative overflow-hidden">
-      <div
-        aria-hidden="true"
-        className="absolute left-[max(-7rem,calc(50%-52rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-          }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-        />
-      </div>
-      <div
-        aria-hidden="true"
-        className="absolute left-[max(45rem,calc(50%+8rem))] top-1/2 -z-10 -translate-y-1/2 transform-gpu blur-2xl"
-      >
-        <div
-          style={{
-            clipPath:
-              "polygon(74.8% 41.9%, 97.2% 73.2%, 100% 34.9%, 92.5% 0.4%, 87.5% 0%, 75% 28.6%, 58.5% 54.6%, 50.1% 56.8%, 46.9% 44%, 48.3% 17.4%, 24.7% 53.9%, 0% 27.9%, 11.9% 74.2%, 24.9% 54.1%, 68.6% 100%, 74.8% 41.9%)",
-          }}
-          className="aspect-[577/310] w-[36.0625rem] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-30"
-        />
-      </div>
+    <AuroraBackground classID="">
       <div className="mb-6 grid items-center justify-center space-x-2 px-6 py-3 md:mb-0 md:flex">
         <AnimatedBars />
         <div className="grid items-center justify-center gap-y-2 space-x-2 text-center text-sm sm:text-base md:inline-flex">
@@ -152,6 +103,6 @@ export default async function NowPlaying() {
           </p>
         </div>
       </div>
-    </div>
+    </AuroraBackground>
   );
 }
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,6 +2,10 @@ import type { Config } from "tailwindcss";
 import baseConfig from "@designali/tailwind-config/web";
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 export default {
   // We need to append the path to the UI package to the content array so that
   // those classes are included correctly.
@@ -13,7 +17,18 @@ export default {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
         mono: ["var(--font-geist-mono)", ...fontFamily.mono],
       },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
       keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
         "shine-pulse": {
           "0%": {
             "background-position": "0% 0%",
@@ -28,4 +43,16 @@ export default {
       },
     },
   },
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
