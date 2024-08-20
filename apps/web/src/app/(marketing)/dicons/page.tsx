4ba8252b6e90f05ddbd751bb8ccfc2aa953bfbd4
Ali Imam <visual.imam@gmail.com>
2024-08-20 12:10:42 +0530

    ho

--- apps/web/src/app/(marketing)/dicons/page.tsx
+++ apps/web/src/app/(marketing)/dicons/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
+
 import { IconGenerator } from "./_components/icon-generator";
 
 export default function Home() {
   return (
     <div className="mt-14">
-      <IconGenerator />
+      <Suspense>
+        <IconGenerator />
+      </Suspense>
     </div>
   );
 }
