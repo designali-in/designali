88565dca4cc4e83bb6225bdfb20c96e510c75f23
Ali Imam <visual.imam@gmail.com>
2024-08-22 23:51:47 +0530

    xc

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import { linkMiddleware } from "@/server/middlewares";
 
 export { auth as middleware } from "@designali/auth";
