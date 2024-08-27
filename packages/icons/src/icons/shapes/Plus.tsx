0cd0987fd2074c62d56682740804afcd1fc475ba
Ali Imam <visual.imam@gmail.com>
2024-08-27 19:23:08 +0530

    ddd

--- packages/icons/src/icons/shapes/Plus.tsx
+++ packages/icons/src/icons/shapes/Plus.tsx
@@ -18,8 +18,8 @@ export const Plus = React.forwardRef<SVGSVGElement, DesignaliProps>(
         {...props}
         ref={forwardedRef}
       >
-        <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
-        <path d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2" />
+        <path d="M5 12h14" />
+        <path d="M12 5v14" />
       </svg>
     );
   },
