2f642ab6dde34135071c2d3fc75d65d2012a6e3f
Ali Imam <visual.imam@gmail.com>
2024-09-06 21:42:32 +0530

    fdbad

---
+++ apps/web/src/app/(marketing)/solutions/branding/page.tsx
@@ -0,0 +1,84 @@
+import Link from "next/link";
+import { Connect } from "@/components/common/connect";
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import PageTitle from "@/src/components/mdx/page-title";
+import { About } from "@/src/components/solutions/products/branding/about";
+import { Banner } from "@/src/components/solutions/products/branding/banner";
+import { BrandingCars } from "@/src/components/solutions/products/branding/card";
+import { BrandingPricing } from "@/src/components/solutions/products/branding/pricing";
+import { Process } from "@/src/components/solutions/products/branding/process";
+import ReviewList from "@/src/components/solutions/review-list";
+import { auth } from "@designali/auth";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@designali/ui/breadcrumb";
+import { Button } from "@designali/ui/button";
+
+export const metadata = {
+  title: "Branding - Designali",
+  description:
+    "This section includes end-to-end guides for developing Next.js 13 apps.",
+};
+
+export default async function Branding() {
+  const product = await getProductBySlug("branding");
+  const session = await auth();
+  return (
+    <div className="mx-auto mb-20 mt-28 max-w-3xl px-6 md:max-w-7xl xl:px-0">
+      <PageTitle
+        title={product.name}
+        description={`I am dedicated to creating premium branding solutions 
+                      exclusively for small enterprises with grand aspirations.`}
+      />
+      <div className="mt-4 flex justify-center">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/solutions">Solutions</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{product.name}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <Banner />
+      <BrandingCars />
+      <Process />
+      <About />
+      <div id="reviews">
+        <div className="flex justify-center">
+          <h1 className="inline-flex items-baseline bg-gradient-to-r from-slate-800 via-slate-600 to-slate-800 bg-clip-text py-6 text-xl font-semibold text-transparent dark:bg-gradient-to-r dark:from-slate-400 dark:via-slate-200 dark:to-slate-400 dark:bg-clip-text sm:text-3xl">
+            Reviews
+          </h1>
+        </div>
+        {session ? (
+          <ReviewList
+            productId={product.id}
+            productSlug={product.slug}
+            userId={session.user.id}
+          />
+        ) : (
+          <Link className="mb-6 flex justify-center" href="/login">
+            <Button variant="outline" size="lg">
+              Login to see the reviews
+              <span className="sr-only">Buy now</span>
+            </Button>
+          </Link>
+        )}
+      </div>
+      <BrandingPricing />
+      <Connect />
+    </div>
+  );
+}
---
+++ apps/web/src/app/(marketing)/solutions/logo/page.tsx
@@ -0,0 +1,81 @@
+import Link from "next/link";
+import { Connect } from "@/components/common/connect";
+import { FAQ } from "@/components/home/faq";
+import { getProductBySlug } from "@/lib/actions/product.actions";
+import PageTitle from "@/src/components/mdx/page-title";
+import { About } from "@/src/components/solutions/products/logo/about";
+import { Banner } from "@/src/components/solutions/products/logo/banner";
+import ReviewList from "@/src/components/solutions/review-list";
+import { Related } from "@/src/components/solutions/services";
+import { auth } from "@designali/auth";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@designali/ui/breadcrumb";
+import { Button } from "@designali/ui/button";
+
+import Branding from "./logo";
+
+export const metadata = {
+  title: "Logo - Designali",
+  description:
+    "This section includes end-to-end guides for developing Next.js 13 apps.",
+};
+
+export default async function Logo() {
+  const product = await getProductBySlug("logo");
+  const session = await auth();
+  return (
+    <div className="mx-auto mb-20 mt-28 max-w-3xl px-6 md:max-w-7xl">
+      <PageTitle title={product.name} description={`✌️ Under Construction`} />
+      <div className="mt-10 flex justify-center">
+        <Breadcrumb>
+          <BreadcrumbList>
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/">Home</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbLink href="/solutions">Services</BreadcrumbLink>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>{product.name}</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <Banner />
+      <About />
+      <Branding />
+      <Related />
+      <div id="reviews">
+        <div className="flex justify-center">
+          <h1 className="inline-flex items-baseline bg-gradient-to-r from-slate-800 via-slate-600 to-slate-800 bg-clip-text py-6 text-xl font-semibold text-transparent dark:bg-gradient-to-r dark:from-slate-400 dark:via-slate-200 dark:to-slate-400 dark:bg-clip-text sm:text-3xl">
+            Reviews
+          </h1>
+        </div>
+        {session ? (
+          <ReviewList
+            productId={product.id}
+            productSlug={product.slug}
+            userId={session.user.id}
+          />
+        ) : (
+          <Link className="mb-6 flex justify-center" href="/login">
+            <Button variant="outline" size="lg">
+              Login to see the reviews
+              <span className="sr-only">Buy now</span>
+            </Button>
+          </Link>
+        )}
+      </div>
+      <FAQ />
+      <Connect />
+    </div>
+  );
+}
