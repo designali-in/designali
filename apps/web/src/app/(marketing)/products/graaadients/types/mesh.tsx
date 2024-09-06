e87cede30539fe304ed0c3e3dc1a8491ddccb549
Ali Imam <visual.imam@gmail.com>
2024-09-06 11:52:42 +0530

    ds

--- apps/web/src/app/(marketing)/products/graaadients/types/mesh.tsx
+++ apps/web/src/app/(marketing)/products/graaadients/types/mesh.tsx
@@ -3,7 +3,7 @@ import cloudinary from "@/lib/cloudinary";
 
 export async function Grad1() {
   const data = await cloudinary.v2.search
-    .expression(`folder:products/graaadients/gradient/*`)
+    .expression(`folder:products/graaadients/*`)
     .sort_by("created_at", "desc")
     .max_results(400)
     .execute();
