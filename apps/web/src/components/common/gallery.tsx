a13eb49283556851fdb42d21435301ac886a06f6
Ali Imam <visual.imam@gmail.com>
2024-09-07 10:36:02 +0530

    dsbsg

--- apps/web/src/components/common/gallery.tsx
+++ apps/web/src/components/common/gallery.tsx
@@ -17,6 +17,7 @@ export function Grid2({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={700}
                 height={600}
                 className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -39,6 +40,7 @@ export function Grid3({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={500}
                 height={500}
                 className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -61,6 +63,7 @@ export function Grid4({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={400}
                 height={400}
                 className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -83,6 +86,7 @@ export function Grid5({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={300}
                 height={300}
                 className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -105,6 +109,7 @@ export function Grid5BG({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={300}
                 height={300}
                 className="rounded-xl border border-slate-200 bg-slate-100 object-cover hover:cursor-zoom-in hover:saturate-0 dark:border-slate-800 dark:bg-slate-900"
@@ -127,6 +132,7 @@ export function Grid6({ images }: { images: any }) {
               <Image
                 src={image.secure_url}
                 alt={image.secure_url}
+                loading="lazy"
                 width={200}
                 height={200}
                 className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -147,6 +153,7 @@ export function IconsHome({ images }: { images: any }) {
           <Image
             src={image.secure_url}
             alt={image.secure_url}
+            loading="lazy"
             width={200}
             height={200}
             className="rounded-xl hover:cursor-zoom-in hover:saturate-0"
@@ -166,6 +173,7 @@ export function ImageZoomTwo({ images }: { images: any }) {
             <Image
               src={image.secure_url}
               alt={image.secure_url}
+              loading="lazy"
               width={700}
               height={200}
               className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
@@ -186,6 +194,7 @@ export function ImageZoomThree({ images }: { images: any }) {
             <Image
               src={image.secure_url}
               alt={image.secure_url}
+              loading="lazy"
               width={700}
               height={200}
               className="rounded-xl object-cover hover:cursor-zoom-in hover:saturate-0"
