eccddeb1ee6e59f537c142a6c8a57048a2f0f496
Ali Imam <visual.imam@gmail.com>
2024-11-06 12:56:55 +0530

    adDAFe

--- apps/www/registry/default/designali/ui/dialog.tsx
+++ apps/www/registry/default/designali/ui/dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "dIcons";
+import { X } from "DIcons";
 
 import { cn } from "@/lib/utils";
 
