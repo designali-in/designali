b55a13b0873b66517ff3742f8977f9624d36b681
Ali Imam <visual.imam@gmail.com>
2024-11-06 12:59:23 +0530

    bkjbjk lnln lknllkl;

--- apps/www/registry/default/designali/ui/dialog.tsx
+++ apps/www/registry/default/designali/ui/dialog.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "DIcons";
+import { X } from "dicons";
 
 import { cn } from "@/lib/utils";
 
