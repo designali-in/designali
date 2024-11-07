80c928a960ea8df31a642c9d72bd9f717275980b
Ali Imam <visual.imam@gmail.com>
2024-11-07 22:05:50 +0530

    sv

--- apps/www/src/app/(marketing)/ui/page.tsx
+++ apps/www/src/app/(marketing)/ui/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { Button } from "@/registry/default/designali/ui/button";
-import { ArrowRight } from "lucide-react";
 
 import "public/registry/themes.css";
 
@@ -13,31 +12,46 @@ export default function HomePage() {
   return (
     <main>
       <div className="mx-auto mt-10 grid max-w-xl grid-cols-2 gap-3">
-        <Link
-          href="/ui/buttons"
-          className="inline-flex w-full items-center justify-between whitespace-nowrap rounded-lg border border-input bg-background p-4 font-bold shadow-sm shadow-black/[0.04] ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-14"
-        >
-          Buttons
-          <Button size="icon">
-            <ArrowRight size={16} strokeWidth={2} className="opacity-60" />
+        <Link href="/ui/buttons">
+          <Button
+            className="flex h-full w-full justify-between gap-8 py-3 text-left"
+            icon="ArrowRight"
+            variant="outline"
+          >
+            <div className="space-y-1">
+              <h3>Buttons</h3>
+              <p className="whitespace-break-spaces font-normal text-muted-foreground">
+                50+ ui components
+              </p>
+            </div>
           </Button>
         </Link>
-        <Link
-          href="/ui/inputs"
-          className="inline-flex w-full items-center justify-between whitespace-nowrap rounded-lg border border-input bg-background p-4 font-bold shadow-sm shadow-black/[0.04] ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-14"
-        >
-          Inputs
-          <Button size="icon">
-            <ArrowRight size={16} strokeWidth={2} className="opacity-60" />
+        <Link href="/ui/inputs">
+          <Button
+            className="flex h-full w-full justify-between gap-8 py-3 text-left"
+            icon="ArrowRight"
+            variant="outline"
+          >
+            <div className="space-y-1">
+              <h3>Inputs</h3>
+              <p className="whitespace-break-spaces font-normal text-muted-foreground">
+                50+ ui components
+              </p>
+            </div>
           </Button>
         </Link>
-        <Link
-          href="/ui/mockups"
-          className="inline-flex w-full items-center justify-between whitespace-nowrap rounded-lg border border-input bg-background p-4 font-bold shadow-sm shadow-black/[0.04] ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring/70 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 sm:h-14"
-        >
-          Mockups
-          <Button size="icon">
-            <ArrowRight size={16} strokeWidth={2} className="opacity-60" />
+        <Link href="/ui/mockups">
+          <Button
+            className="flex h-full w-full justify-between gap-8 py-3 text-left"
+            icon="ArrowRight"
+            variant="outline"
+          >
+            <div className="space-y-1">
+              <h3>Mockups</h3>
+              <p className="whitespace-break-spaces font-normal text-muted-foreground">
+                5+ ui apple mockups
+              </p>
+            </div>
           </Button>
         </Link>
       </div>
