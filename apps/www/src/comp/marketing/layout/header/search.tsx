6b2cbd10a6cf00812b74b503aaf9dd36a732cbc5
Ali Imam <visual.imam@gmail.com>
2024-12-12 18:00:04 +0530

    search

--- apps/www/src/comp/marketing/layout/header/search.tsx
+++ apps/www/src/comp/marketing/layout/header/search.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { Skeleton } from "@/registry/default/ui/skeleton";
-import { formatUrl } from "@/src/lib/utils";
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import { DIcons } from "dicons";
-import { useDebounce } from "use-debounce";
 
 import { useCopyToClipboard } from "@/hooks/use-copy-clipboard";
 import { Button } from "@/components/ui/button";
@@ -34,8 +27,6 @@ type Groups = {
 const CommandMenu = () => {
   const [open, setOpen] = React.useState(false);
   const [copy] = useCopyToClipboard();
-  const [query, setQuery] = useState("");
-  const router = useRouter();
 
   React.useEffect(() => {
     const down = (e: KeyboardEvent) => {
@@ -48,44 +39,6 @@ const CommandMenu = () => {
     document.addEventListener("keydown", down);
     return () => document.removeEventListener("keydown", down);
   }, []);
-
-  const [data, setData] = useState<{ id: string; name: string }[]>([]);
-  const debouncedQuery = useDebounce(query, 300);
-
-  useEffect(() => {
-    if (debouncedQuery.length > 0) {
-      refetch();
-    }
-  }, [debouncedQuery]);
-
-  const {
-    data: queryResults,
-    refetch,
-    isFetching,
-  } = useQuery({
-    queryFn: async () => {
-      if (!query) return [];
-
-      const trimmedQuery = query.trimEnd();
-
-      const { data } = await axios(`/api/graphic/search?q=${trimmedQuery}`);
-
-      return data as { id: string; name: string }[];
-    },
-    queryKey: ["search-query"],
-    enabled: false, //by default it will not fetch
-  });
-
-  useEffect(() => {
-    if (!query) {
-      setData([]);
-      return;
-    }
-
-    if (queryResults) {
-      setData(queryResults);
-    }
-  }, [queryResults, query]);
 
   const openLink = React.useCallback((url: string) => {
     setOpen(false);
@@ -170,42 +123,9 @@ const CommandMenu = () => {
         icon="Command"
       ></Button>
       <CommandDialog open={open} onOpenChange={setOpen}>
-        <CommandInput
-          placeholder="Search graphic..."
-          value={query}
-          onValueChange={setQuery}
-        />
+        <CommandInput placeholder="Type a command or search..." />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
-          {isFetching ? (
-            <div className="space-y-1 overflow-hidden p-2">
-              <Skeleton className="h-8 rounded-sm" />
-              <Skeleton className="h-8 rounded-sm" />
-            </div>
-          ) : (
-            data.length > 0 && (
-              <CommandGroup heading="Anime">
-                {data.map((queryItem) => {
-                  const formattedHref = `/graphic/${formatUrl(queryItem.name)}`;
-
-                  return (
-                    <a href={formattedHref} key={queryItem.id}>
-                      <CommandItem
-                        className="cursor-pointer"
-                        onSelect={() => {
-                          setOpen(false);
-                          setQuery("");
-                          router.push(formattedHref);
-                        }}
-                      >
-                        {queryItem.name}
-                      </CommandItem>
-                    </a>
-                  );
-                })}
-              </CommandGroup>
-            )
-          )}
           {groups.map((group, i) => (
             <React.Fragment key={group.name}>
               <CommandGroup heading={group.name}>
