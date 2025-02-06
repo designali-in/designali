b7e7b04d10496330af7f3ce528d1a838c01d0457
Ali Imam <visual.imam@gmail.com>
2025-02-06 09:16:29 +0530

    oil

--- apps/www/src/app/(marketing)/products/graaadients/grads.tsx
+++ apps/www/src/app/(marketing)/products/graaadients/grads.tsx
@@ -6,14 +6,18 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Halftone } from "./types/halftone";
 import { Neon } from "./types/neon";
 import { Noise } from "./types/noise";
+import { OilPaint } from "./types/oil-paint";
 import { Solarize } from "./types/solarize";
 
 export function Grads() {
   return (
-    <Tabs defaultValue="5" className="items-center justify-center text-center">
+    <Tabs defaultValue="6" className="items-center justify-center text-center">
       <TabsList className="sticky top-24 z-20 mb-6 w-full items-center justify-center text-center  md:w-auto">
         <ScrollArea className="whitespace-nowrap">
           <div className="space-x-2">
+            <TabsTrigger value="6" className="px-6">
+              Oil Paint
+            </TabsTrigger>
             <TabsTrigger value="5" className="px-6">
               Neon
             </TabsTrigger>
@@ -33,6 +37,9 @@ export function Grads() {
           <ScrollBar orientation="horizontal" />
         </ScrollArea>
       </TabsList>
+      <TabsContent value="6">
+        <OilPaint />
+      </TabsContent>
       <TabsContent value="5">
         <Neon />
       </TabsContent>
--- apps/www/src/comp/marketing/agency/pricing.tsx
+++ apps/www/src/comp/marketing/agency/pricing.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 export default function FUIPricingWithSpecialTwo() {
   return (
     <div className="relative isolate mt-10 overflow-hidden bg-transparent">
-      <div className="absolute inset-0 -z-10 h-[600px] w-full bg-transparent bg-[linear-gradient(to_right,#57534e_1px,transparent_1px),linear-gradient(to_bottom,#57534e_1px,transparent_1px)] bg-[size:6rem_4rem] opacity-10 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] dark:bg-[linear-gradient(to_right,#a8a29e_1px,transparent_1px),linear-gradient(to_bottom,#a8a29e_1px,transparent_1px)]"></div>
+      <div className="absolute inset-0 -z-10 h-[600px] w-full bg-transparent bg-[linear-gradient(to_right,#57534e_1px,transparent_1px),linear-gradient(to_bottom,#57534e_1px,transparent_1px)] bg-[size:6rem_4rem] opacity-20 [mask-image:radial-gradient(ellipse_80%_50%_at_50%_0%,#000_70%,transparent_110%)] dark:bg-[linear-gradient(to_right,#a8a29e_1px,transparent_1px),linear-gradient(to_bottom,#a8a29e_1px,transparent_1px)]"></div>
       <div className="mx-auto max-w-7xl px-6 pb-96 pt-24 text-center sm:pt-32 lg:px-8">
         <div className="flex justify-center py-10">
           <DIcons.IndianRupee
@@ -18,12 +18,16 @@ export default function FUIPricingWithSpecialTwo() {
           />
         </div>
         <div className="mx-auto grid max-w-3xl items-center justify-center px-8 pb-1 text-center">
-          <h1>Why Choose Me?</h1>
-          <p>
-            Get unlimited, high-quality designs with a fast turnaround and
-            unlimited revisions—all for a flat monthly rate. No hiring hassles,
-            no hidden fees—just top-tier creativity on demand. 🚀
-          </p>
+          <div className="space-y-10 py-10">
+            <h1 className="text-ali text-xl uppercase tracking-wider">
+              Why Choose Me?
+            </h1>
+            <p className="text-sm text-primary/70">
+              Get unlimited, high-quality designs with a fast turnaround and
+              unlimited revisions—all for a flat monthly rate. No hiring
+              hassles, no hidden fees—just top-tier creativity on demand. 🚀
+            </p>
+          </div>
           <h3
             className={cn(
               Avegra.className,
